870c97ac532706505ab3e732af730adf53b63f10
Periyannan35 <periyannanmarch@gmail.com>
2025-12-17 21:24:33 +0530

    Update model_loader.py

--- src/model_loader.py
+++ src/model_loader.py
@@ -1,1 +1,11 @@
 
+from tensorflow.keras.models import load_model
+
+def load_cnn_model(model_path: str):
+    """
+    Loads and returns the trained CNN model.
+
+    Single Responsibility:
+    This function only handles model loading.
+    """
+    return load_model(model_path)
